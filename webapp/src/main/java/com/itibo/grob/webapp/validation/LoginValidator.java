2b97934ce87af196747e06d61e02bcc7e7d8ce40
Union One <hax2033@gmail.com>
2016-03-28 20:43:12 +0300

    Create first validating in registration

---
+++ webapp/src/main/java/com/itibo/grob/webapp/validation/LoginValidator.java
@@ -0,0 +1,24 @@
+package com.itibo.grob.webapp.validation;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@FacesValidator("login")
+public class LoginValidator implements Validator {
+    private static final String LOGIN_PATTERN = "^[a-zA-Z]{3,16}$";
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+        Pattern pattern = Pattern.compile(LOGIN_PATTERN);
+        Matcher matcher = pattern.matcher(o.toString());
+        if (!matcher.matches()) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login is not ok", null));
+        }
+    }
+}
