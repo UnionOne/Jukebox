b9c7ef89ed80a094fc28f89e0a22047a58422a5c
Union One <hax2033@gmail.com>
2016-03-28 09:20:56 +0300

    Added GenericServiceTest methods

--- services/src/test/java/com/itibo/grob/services/common/GenericServiceTest.java
+++ services/src/test/java/com/itibo/grob/services/common/GenericServiceTest.java
@@ -1,7 +1,9 @@
 package com.itibo.grob.services.common;
 
 import org.apache.commons.lang3.RandomUtils;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.Sort;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public abstract class GenericServiceTest<T extends Persistable<ID>, ID extends Serializable,
@@ -61,5 +64,144 @@ public abstract class GenericServiceTest<T extends Persistable<ID>, ID extends S
         return getFieldsNames().get(RandomUtils.nextInt(0, getFieldsNames().size()));
     }
 
+    @Test
+    public void countEtitiesTest() {
+        long before = service.count();
+        service.save(entity);
+        long after = service.count();
+        Assert.assertEquals(before + 1, after);
+    }
 
+    @Test
+    public void deleteEntityByIdTest() {
+        ID saveId = service.save(entity).getId();
+        service.delete(saveId);
+        Assert.assertFalse(service.exists(saveId));
+    }
+
+    @Test
+    public void deleteSequenceOfEntitiesTest() {
+        Iterable<? extends T> saved = service.save(entities);
+        List<ID> ids = new ArrayList<>();
+
+        for (T t : saved) {
+            ids.add(t.getId());
+        }
+
+        service.delete(saved);
+        Assert.assertEquals(Collections.EMPTY_LIST, service.findAll(ids));
+    }
+
+    @Test
+    public void deleteEntityTest() {
+        T saved = service.save(entity);
+        service.delete(saved);
+        Assert.assertFalse(service.exists(saved.getId()));
+    }
+
+    @Test
+    public void deleteAllEntitiesTest() {
+        service.save(entity);
+        service.deleteAll();
+        Assert.assertTrue(service.count() == 0);
+    }
+
+    @Test
+    public void deleteAllEntitiesInBatchTest() {
+        service.save(entity);
+        service.deleteAllInBatch();
+        Assert.assertTrue(service.count() == 0);
+    }
+
+    @Test
+    public void deleteSequenceOfEntitiesInBatchTest() {
+        @SuppressWarnings("unchecked")
+        Iterable<T> saved = (Iterable<T>) service.save(entities);
+        List<ID> ids = new ArrayList<>();
+
+        for (T t : saved) {
+            ids.add(t.getId());
+        }
+
+        service.deleteInBatch(saved);
+        Assert.assertEquals(Collections.EMPTY_LIST, service.findAll(ids));
+    }
+
+    @Test
+    public void entityExistenceByIdTest() {
+        T saved = service.save(entity);
+        Assert.assertTrue(service.exists(saved.getId()));
+    }
+
+    @Test
+    public void findAllEntitiesTest() {
+        service.deleteAll();
+        Iterable<? extends T> saved = service.save(entities);
+        Iterable<T> found = service.findAll();
+        Assert.assertEquals(saved, found);
+    }
+
+    @Test
+    public void findAllEntitiesByIds() {
+        Iterable<? extends T> saved = service.save(entities);
+        List<ID> ids = new ArrayList<>();
+
+        for (T t : saved) {
+            ids.add(t.getId());
+        }
+
+        Iterable<T> found = service.findAll(ids);
+        Assert.assertEquals(saved, found);
+    }
+
+    @Test
+    public void findAllEntititesByPageableTest() {
+        Iterable<T> found = service.findAll(sort);
+        service.delete(found);
+        Assert.assertEquals(Collections.EMPTY_LIST, found);
+    }
+
+    @Test
+    public void findAllEntitiesBySortTest() {
+        Iterable<T> found = service.findAll(sort);
+        service.delete(found);
+        Assert.assertEquals(Collections.EMPTY_LIST, service.findAll(sort));
+    }
+
+    @Test
+    public void findOneEntityByIdTest() {
+        service.save(entity);
+        T found = service.findOne(entity.getId());
+        Assert.assertEquals(found == null, !service.exists(entity.getId()));
+    }
+
+    @Test
+    public void flushPendingChangesTest() {
+        service.flush();
+    }
+
+    @Test
+    public void saveSequenceOfEntitiesTest() {
+        Iterable<? extends T> saved = service.save(entities);
+
+        List<ID> ids = new ArrayList<>();
+
+        for (T t : saved) {
+            ids.add(t.getId());
+        }
+
+        Assert.assertEquals(saved, service.findAll(ids));
+    }
+
+    @Test
+    public void saveOneEntityTest() {
+        T saved = service.save(entity);
+        Assert.assertEquals(entity, service.findOne(saved.getId()));
+    }
+
+    @Test
+    public void saveOneEntityAndFlushTest() {
+        T saved = service.save(entity);
+        Assert.assertEquals(entity, service.findOne(saved.getId()));
+    }
 }
